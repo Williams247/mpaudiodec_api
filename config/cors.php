4974e027e370018500ab2b496a2109b7f20d74aa
Williams Alex <godswillwilliams9@gmail.com>
2026-04-16 10:51:02 +0100

    Update: Added cors for vercel

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => array_values(array_filter(array_map(
         'trim',
-        explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000'))
+        explode(',', env('CORS_ALLOWED_ORIGINS', 'https://audiodec.vercel.app,http://localhost:3000,http://127.0.0.1:3000'))
     ))),
     'allowed_origins_patterns' => [
         '/^https?:\/\/localhost(:\d+)?$/',
